79495e3006e5abbac84c5a2245f27ebe4542b626
hrishikeshps94 <hrishikeshps94@gmail.com>
2024-08-08 19:00:42 +0530

    chore: Add performance analysis function for evaluating models on validation dataset

--- onnx_converter.py
+++ onnx_converter.py
@@ -5,7 +5,13 @@ from onnxruntime.quantization import quantize_dynamic
 from config import logger
 # Assuming these imports are from your project structure
 from config import TrainConfig
-from inference import load_model
+from inference import load_model, run_onnx_model
+import onnxruntime
+from eval import calculate_metrics
+import tqdm
+from data import CustomDataset, collate_fn, BatchSampler
+from torch.utils.data import DataLoader
+from transformers import AutoTokenizer
 
 
 def create_dummy_inputs(batch_size: int = 1, seq_length: int = 512) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
@@ -63,8 +69,92 @@ def quantize_onnx_model(input_path: str, output_path: str):
         raise
 
 
+def val_dataset(tokenizer):
+    """
+    Create a validation dataset loader.
+
+    Args:
+        tokenizer (Tokenizer): The tokenizer object to use for tokenizing the data.
+
+    Returns:
+        DataLoader: The validation dataset loader.
+
+    """
+    val_dataset = CustomDataset(TrainConfig.data_path, tokenizer, mode='test')
+    val_sampler = BatchSampler(
+        val_dataset, batch_size=1, keep_last_batch=True, shuffle=False)
+    val_loader = DataLoader(
+        val_dataset, batch_sampler=val_sampler, collate_fn=collate_fn)
+    return val_loader
+
+
+def evaluate(model_path, val_loader):
+    """
+    Evaluate the model on the validation dataset.
+
+    Args:
+        model_path (str): The path to the model file.
+        val_loader (torch.utils.data.DataLoader): The validation data loader.
+
+    Returns:
+        dict: A dictionary containing the evaluation results.
+    """
+    logger.info("Evaluating the model on the validation dataset...")
+    mode = "torch"
+    if model_path.endswith(".onnx"):
+        mode = "onnx"
+        model = onnxruntime.InferenceSession(
+            model_path, providers=["CPUExecutionProvider"])
+    if model_path.endswith(".pt"):
+        model, _ = load_model(model_path)
+        model = model.to(device)
+        model.eval()
+    final_predictions = {}
+    for idx, batch in enumerate(tqdm.tqdm(val_loader)):
+        batch = {k: v.to(device) for k, v in batch.items()}
+        if mode == "onnx":
+            outputs = run_onnx_model(model,
+                                     [batch["input_ids"], batch["bbox"], batch["attention_mask"]])
+            outputs = torch.nn.functional.softmax(outputs, dim=-1)
+        else:
+            with torch.no_grad():
+                outputs = model(**batch, return_dict=True)
+                outputs = torch.nn.functional.softmax(outputs.logits, dim=-1)
+        predictions = outputs.argmax(-1)
+        final_predictions.setdefault('predictions', []).extend(predictions)
+        final_predictions.setdefault('labels', []).extend(batch["labels"])
+    results = calculate_metrics(
+        torch.cat(final_predictions['predictions']), torch.cat(final_predictions['labels']), TrainConfig.class_list)
+    return results
+
+
+def performance_analyser():
+    """
+    Analyzes the performance of different models by evaluating them on a validation dataset.
+
+    This function evaluates the performance of three models: the original model, the ONNX model, and the quantized ONNX model.
+    It uses the `evaluate` function to evaluate each model on a validation dataset and logs the results.
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
+    org_model_path = 'weights/best.pt'
+    onnx_model_path = 'weights/best.onnx'
+    quantized_model_path = 'weights/quantized_best.onnx'
+    models = [org_model_path, onnx_model_path, quantized_model_path]
+    tokenizer = AutoTokenizer.from_pretrained(
+        TrainConfig.base_model_name)
+    val_loader = val_dataset(tokenizer)
+    for model_path in models:
+        logger.info(f"Evaluating model: {model_path}")
+        result = evaluate(model_path, val_loader)
+        logger.info(f"Model results: {result}")
+
+
 def main():
-    device = "cpu" if torch.cuda.is_available() else "cpu"
     try:
         # Load the model
         model, _ = load_model(model_path='weights/best.pt')
@@ -84,4 +174,6 @@ def main():
 
 
 if __name__ == "__main__":
+    device = "cpu" if torch.cuda.is_available() else "cpu"
     main()
+    performance_analyser()
